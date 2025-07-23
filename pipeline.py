dc343c06150809248bbe3032d1d390a0411d0f8c
saurluca <spuckteller@gmail.com>
2025-07-23 14:04:53 +0200

    refactor: extract question generator

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,10 @@
 # %%
 import dspy
 from dotenv import load_dotenv
-from text_processing import extract_text_from_file
-from db_utils import (
-    get_document_content_from_db,
-    save_key_points_to_db,
-    save_questions_to_db,
-)
+from db_utils import get_document_content_from_db
 from teacher import evaluate_student_answer
 from summarizer import summarise_document
+from question_generator import generate_questions
 
 
 load_dotenv()
@@ -36,44 +32,14 @@ document = get_document_content_from_db(doc_id)
 
 key_points = summarise_document(doc_id)
 
+print(key_points)
 # %% GENERATE QUESTIONS
 
-
-class Question(dspy.Signature):
-    """Generate multiple choice questions, answer options, and correct answer indices from key points."""
-
-    key_points: str = dspy.InputField(description="The key points of the document.")
-
-    questions: list[str] = dspy.OutputField(
-        description="A list of multiple choice questions generated from the key points."
-    )
-    answer_options: list[list[str]] = dspy.OutputField(
-        description="A list of answer options for each question. Each entry is a list of 4 answer strings."
-    )
-    correct_answers: list[int] = dspy.OutputField(
-        description="A list of integers, each representing the index (0-3) of the correct answer in the answer options for each question."
-    )
-
-
-question_generator = dspy.ChainOfThought(Question)
-
-qg_response = question_generator(key_points=key_points)
+qg_response = generate_questions(doc_id)
 
 print(qg_response.questions[0])
 print(qg_response.answer_options[0])
 print("Correct answer: ", qg_response.correct_answers[0])
-
-# Save generated questions to the database
-save_questions_to_db(
-    doc_id,
-    qg_response.questions,
-    qg_response.answer_options,
-    qg_response.correct_answers,
-)
-
-# TODO: verify results: 1 answer correct, 3 wrong, answer is correct, results relevant
-# TODO: also return the keypoint the answer was based on? For explainability, or for teacher.
-# TODO: experiment wiht more nested data structure so answer / questions togeher. maybe easier for model to produce.
 
 ### ASK QUESTION
 
--- summariser.py
+++ summariser.py
@@ -2,15 +2,13 @@ import dspy
 from db_utils import save_key_points_to_db, get_document_content_from_db
 
 
-def summarise_document(doc_id, save_to_db=True):
+def summarise_document(doc_id):
     document = get_document_content_from_db(doc_id)
     
     summarizer = dspy.ChainOfThought("document -> key_points")
 
     response = summarizer(document=document)
-
-    if save_to_db:
-        # save key points to db
-        save_key_points_to_db(doc_id, response.key_points)
+    
+    save_key_points_to_db(doc_id, response.key_points)
 
     return response.key_points

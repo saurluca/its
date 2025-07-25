0339a6d400a833e85ba496785c07ddfd7576f191
GrafKnuspy <spuckteller@gmail.com>
2025-07-25 10:16:59 +0200

    remove old api files

--- frontend/old/server/api/courses/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { courses } from "../../database/schema"
-import { eq } from "drizzle-orm"
-
-export default defineEventHandler(async (event) => {
-    const { secure } = await requireUserSession(event)
-    if (!secure) throw createError({ statusCode: 401, message: "Unauthorized" })
-
-    return await useDrizzle().select().from(courses).where(eq(courses.organisationId, secure.organisationId)).limit(100)
-}) 
--- frontend/old/server/api/tasks/index.get.ts
+++
@@ -1,9 +0,0 @@
-import { tasks } from "../../database/schema"
-import { eq } from "drizzle-orm"
-
-export default defineEventHandler(async (event) => {
-    const { secure } = await requireUserSession(event)
-    if (!secure) throw createError({ statusCode: 401, message: "Unauthorized" })
-
-    return await useDrizzle().select().from(tasks).where(eq(tasks.organisationId, secure.organisationId)).limit(100)
-}) 

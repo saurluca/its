fa842f067a129e1d5de99e6515fb5d90d4fd467e
GrafKnuspy <spuckteller@gmail.com>
2025-08-04 10:26:19 +0200

    feat: db user model

---
+++ backend/auth/models.py
@@ -0,0 +1,14 @@
+import uuid
+from datetime import datetime
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    username: str = Field(unique=True, index=True)
+    email: str | None = Field(default=None, index=True)
+    full_name: str | None = Field(default=None)
+    disabled: bool = Field(default=False)
+    hashed_password: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)

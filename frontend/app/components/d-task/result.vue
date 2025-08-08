0da45b3fcc8cd0a1ab4672ae15c797b49a8a307b
saurluca <spuckteller@gmail.com>
2025-08-08 10:29:45 +0200

    style: open text response

--- frontend/app/components/d-task/result.vue
+++ frontend/app/components/d-task/result.vue
@@ -5,7 +5,7 @@ const props = defineProps<{
   task: Task;
   index: number;
   userAnswer: string;
-  isCorrect: boolean;
+  status: "correct" | "partial" | "incorrect";
   feedback: string;
 }>();
 </script>
@@ -14,27 +14,30 @@ const props = defineProps<{
   <div class="bg-white p-6 rounded-lg shadow">
     <div class="flex justify-between">
       <h3 class="text-lg font-medium">Task {{ index + 1 }}</h3>
-      <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium" :class="isCorrect ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
-        ">
-        {{ isCorrect ? "Correct" : "Incorrect" }}
+      <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium" :class="{
+        'bg-green-100 text-green-800': status === 'correct',
+        'bg-yellow-100 text-yellow-800': status === 'partial',
+        'bg-red-100 text-red-800': status === 'incorrect',
+      }">
+        {{ status === 'correct' ? 'Correct' : (status === 'partial' ? 'Partially correct' : 'Incorrect') }}
       </span>
     </div>
-    <!-- <p class="mt-2 text-gray-700">{{ task.question }}</p> -->
 
     <div class="mt-4">
       <p class="text-gray-700">
         <span class="font-semibold">Your answer: </span>
         <span :class="{
           'font-semibold': true,
-          'text-green-600': isCorrect,
-          'text-red-600': !isCorrect,
+          'text-green-600': status === 'correct',
+          'text-yellow-700': status === 'partial',
+          'text-red-600': status === 'incorrect',
         }">{{ userAnswer || "Not answered" }}</span>
       </p>
-      <p v-if="!isCorrect" class="text-gray-700 mt-1">
+      <p v-if="status !== 'correct' && task.type === 'multiple_choice'" class="text-gray-700 mt-1">
         <span class="font-semibold">Correct answer: </span>
         <span class="">{{task.answer_options.find(option => option.is_correct)?.answer || 'Not available'}}</span>
       </p>
-      <p v-if="!isCorrect" class="text-gray-700 mt-4">
+      <p v-if="status !== 'correct'" class="text-gray-700 mt-4">
         <span class="font-semibold">Explanation: </span>
         <span class="">{{ feedback }}</span>
       </p>

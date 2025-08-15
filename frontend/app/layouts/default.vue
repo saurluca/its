38331d9c00875996ecc505078057d3f4c634d420
saurluca <spuckteller@gmail.com>
2025-08-15 11:52:02 +0200

    style: fix scrollbar

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex h-full max-h-screen flex-col sm:flex-row bg-gray-50">
     <DPageSidebar />
-    <main class="flex-1 overflow-scroll">
+    <main class="flex-1 overflow-y-auto">
       <slot></slot>
     </main>
     <DSnackbar />

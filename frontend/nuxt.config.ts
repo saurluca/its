3e4519b39e0b0d71e30b2cf845c542feb5c24dda
saurluca <spuckteller@gmail.com>
2025-08-18 12:40:28 +0200

    cicd: change deployment url

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000", // Default for dev
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || "https://api.dodoit.net",
     },
   },
 

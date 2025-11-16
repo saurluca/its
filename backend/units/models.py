a3a124bb38647905c36143cc95fabcd6d650206d
DarthDanAmesh <danielamemba@gmail.com>
2025-11-16 19:02:19 +0100

    UnitTaskEvent

--- backend/units/models.py
+++ backend/units/models.py
@@ -2,18 +2,29 @@ from typing import TYPE_CHECKING
 from sqlmodel import SQLModel, Field, Relationship
 from uuid import UUID, uuid4
 from datetime import datetime
-
+from enum import Enum
 
 if TYPE_CHECKING:
     from repositories.models import Repository
     from tasks.models import Task
 
+class UnitTaskAction(str, Enum):
+    ADDED = "added"
+    REMOVED = "removed"
 
 class UnitTaskLink(SQLModel, table=True):
     unit_id: UUID | None = Field(default=None, foreign_key="unit.id", primary_key=True)
     task_id: UUID | None = Field(default=None, foreign_key="task.id", primary_key=True)
     created_at: datetime = Field(default_factory=datetime.now)
     deleted_at: datetime | None = None
+
+class UnitTaskEvent(SQLModel, table=True):
+    id: UUID | None = Field(default_factory=uuid4, primary_key=True)
+    unit_id: UUID = Field(foreign_key="unit.id", index=True)
+    task_id: UUID = Field(foreign_key="task.id", index=True)
+    action: UnitTaskAction
+    user_id: UUID = Field(foreign_key="user.id")
+    created_at: datetime = Field(default_factory=datetime.utcnow, index=True)
 
 
 class UnitBase(SQLModel):
